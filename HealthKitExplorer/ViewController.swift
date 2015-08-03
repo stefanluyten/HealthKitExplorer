030a5bcd3ecff64d2416a72b5462d6be2d9264e3
Stefan Luyten <stefan@coruscant.local>
2015-08-03 21:30:49 +0200

    Initial Commit

---
+++ HealthKitExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKitExplorer
+//
+//  Created by Stefan Luyten on 3/08/15.
+//  Copyright © 2015 Triton Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
